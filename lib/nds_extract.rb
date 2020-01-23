03b55b1a2f4c3176c4641f881f279199eae3c850
Paul Kim <pkim1295@gmail.com>
2020-01-23 18:21:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -126,4 +126,5 @@ def studios_totals(nds)
   a_o_a_movies_with_director_names = movies_with_directors_set(nds)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
   return gross_per_studio(movies_with_director_names)
+  binding.pry
 end
